696df3a992e50a9439e9f1f784c9902c1fc7d596
18jelias <jacquie19_e@yahoo.com>
2018-04-02 21:34:11 -0500

    fixed duplicate menu declaration

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/main.jsx';
-import Menu from './components/menu.jsx';
 import {
   BrowserRouter as Router,
   Route,
