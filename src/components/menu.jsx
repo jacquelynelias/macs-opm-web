c80bfb88b14ad572b49ca0714fb370ebf3cea72c
18jelias <jacquie19_e@yahoo.com>
2018-04-30 22:21:30 -0500

    Worked on UI of menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -7,7 +7,7 @@ import {
     Link,
     Switch
 } from 'react-router-dom'
-import { Button, Row, Col } from 'react-bootstrap';
+import { Button, Row, Col, Navbar, NavItem, Nav, MenuItem, NavDropdown } from 'react-bootstrap';
 
 var categories = [
     {
@@ -42,28 +42,57 @@ var pizza = {}
 
 
 class Menu extends Component {
+    constructor(props) {
+        super(props)
+        this.navbar = this.navbar.bind(this)
+    }
+    /* Displays navigation bar */
+    navbar = () => {
+        return (
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                    <img src={require('../assets/logo.png')} height="30" alt=""/>
+                </a>
+                 <a href="#">
+                    <i class="material-icons">shopping_cart</i>
+                </a>
+            </nav>
+        )
+    }
+
 
     render() {
             return (
-                <div className="container">
-                    <div className="row">
-                        <h1>Menu</h1>
-                        <Button>Continue</Button>
-                    </div>
-                    <div className="row">
-                        <div className=".col-12 col-md-4">
-                            {categories.map(function (category) {
-                                return (
-                                    <div className="card bg-dark text-white">
-                                        <div className="card-img-overlay">
-                                            {category.name}
+                <div class="screen">
+                    <div className="container-fluid">
+                        {this.navbar}
+                        <div className="row">
+                            <img src={require('../assets/smu.jpg')} width="100%" alt=""/>
+                            <h1>Menu</h1>
+                            <Button>Continue</Button>
+                        </div>
+                        <div className="row options">
+                            <div className="col-md-6 col-sm-6 col-xs-12">
+                                {categories.map(function (category) {
+                                    return (
+                                        <div className="card bg-dark text-white">
+                                            <div class="menu-item">
+                                                <div className="card-img-overlay">
+                                                    <div className="row">
+                                                        <h3>{category.name}</h3>
+                                                    </div>
+                                                    <div className="row">
+                                                        <button type="button" class="btn btn-outline-danger" onclick={console.log("hi")}>Order ></button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <img className="card-img-bottom" src={category.img} />
                                         </div>
-                                        <img className="card-img-bottom" src={category.img} />
-                                    </div>
 
-                                )
-                            })}
+                                    )
+                                })}
 
+                            </div>
                         </div>
                     </div>
                 </div>
