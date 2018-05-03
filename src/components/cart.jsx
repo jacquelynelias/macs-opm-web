15c402b6398189832aeea4a0cd5daccb67fcf29b
arslan0060 <amemon@smu.edu>
2018-05-02 19:12:52 -0500

    updated cart

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Footer from './footer'
 import './cart.css'
 import {
     BrowserRouter as Router,
@@ -48,8 +49,15 @@ import {
                 quantity: 8
             }
         ]
-    }
-
+    }  
+     getTotalCost() {
+        var total = 0.0;
+        this.state.cartItems.map(function (item){
+            total = total+item.cost
+        });
+        return total
+    }  
+   
     navbar = () => {
         return (
             <nav className="navbar navbar-light bg-light">
@@ -58,7 +66,7 @@ import {
                     <img src={require('../assets/logo.png')} height="40" alt=""/>
                 </a>
                  <a href="/cart">
-                    <i class="material-icons" >shopping_cart</i>
+                    <i className="material-icons" >shopping_cart</i>
                 </a>
             </nav>
         )
@@ -67,50 +75,63 @@ import {
         return (
             <div className="fluid-container">
                 {this.navbar()}
-            <div className="row">
-            <div className="col-md-12">
-            <div className="table-responsive">
-                <table className="table table-striped ml-4">
-                <thead>
-    <tr className ="row">
-      <th className="col-8" >Item</th>
-      <th className="col-4" >Price</th>
-    </tr></thead>
-                    <tbody >
-                    {this.state.cartItems.map(function (item) {
-                    return(
-                    <tr className="row">
-                   
-                        <td width="col-5">
-                        <img src={item.img}/>
-                        </td>
-                        <td className="desc col-3">
-                            <h3>
-                            {item.name}
-                            </h3>
-                            <p className="small">
-                             {item.desc}
-                            </p>
-                            <dl className="small m-b-none">
-                                <dt>Description lists</dt>
-                                <dd>A description list is perfect for defining terms.</dd>
-                            </dl>
-                        </td>
-                        <td className="col-4">
-                           <h1 className="text-center" > ${item.cost}</h1>
-                        </td>
-                    </tr>
-                   
-                
-                    )})}
-                    </tbody>
-                </table>
-            </div>
-            </div>
-            <div className="col-md-4">
-            </div>
-            </div>
+                <div className="row  ml-4 mt-4 mr-4">
+                    <div className="col-md-8">
+                        <div className="table-responsive">
+                            <table className="table table-striped">
+                                <thead>
+                                    <tr className="row">
+                                        <th className="col-8" >Item</th>
+                                        <th className="col-4" >Price</th>
+                                    </tr></thead>
+                                <tbody >
+                                    {this.state.cartItems.map(function (item) {
+                                        return (
+                                            <tr className="row" key={item.id}>
+
+                                                <td width="col-5">
+                                                    <img src={item.img} />
+                                                </td>
+                                                <td className="desc col-3">
+                                                    <h3>
+                                                        {item.name}
+                                                    </h3>
+                                                    <p className="small">
+                                                        {item.desc}
+                                                    </p>
+                                                    <dl className="small m-b-none">
+                                                        <dt>Description lists</dt>
+                                                        <dd>A description list is perfect for defining terms.</dd>
+                                                    </dl>
+                                                </td>
+                                                <td className="col-4 ml-2">
+                                                    <h1 className="text-center alert" > ${item.cost}</h1>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card checkout">
+                            <div className="card-body " >
+                                <h3 className="card-title">Summary</h3>
+                                <div className="card-subtitle mb-2 text-muted">
+                                    <h5>Subtotal: ${this.getTotalCost()}</h5>
+                                    <h6>Tax: ${(this.getTotalCost() * .0875).toFixed(2)}</h6>
+                                    <hr />
+                                    <h4>Total: ${(this.getTotalCost()*1.0875).toFixed(2) }</h4>
+                                    <button className="button btn-primary align-center text-white btn-block">Checkout</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <Footer/>
+
+            </div>
 
         )
     }
